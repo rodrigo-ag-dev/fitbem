48ad7b6011744ddbcdecdda7cfee0a2e0fa9f138
Rodrigo Gonçalves <rodrigo.ag.dev@gmail.com>
2023-10-17 15:31:14 -0300

    Banco local em sqlite3

--- back/knexfile.js
+++ back/knexfile.js
@@ -3,22 +3,44 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: process.env.NODE_DB_CLIENT,
-    connection: process.env.NODE_DB_CONNECTION,
+    connection: process.env.NODE_DB_CONNECTION ? process.env.NODE_DB_CONNECTION : { filename: process.env.NODE_DB_FILENAME },
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   },
 
   staging: {
     client: process.env.NODE_DB_CLIENT,
-    connection: process.env.NODE_DB_CONNECTION,
+    connection: process.env.NODE_DB_CONNECTION ? process.env.NODE_DB_CONNECTION : { filename: process.env.NODE_DB_FILENAME },
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   },
 
   production: {
     client: process.env.NODE_DB_CLIENT,
-    connection: process.env.NODE_DB_CONNECTION,
+    connection: process.env.NODE_DB_CONNECTION ? process.env.NODE_DB_CONNECTION : { filename: process.env.NODE_DB_FILENAME },
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   }
 };
+
+// module.exports = {
+//   development: {
+//     client: 'sqlite3',
+//     connection: { filename: './src/database/sqlite.db' },
+//     migrations: { directory: './src/database/migrations' },
+//     useNullAsDefault: true,
+//   },
+//   staging: {
+//     client: 'sqlite3',
+//     connection: { filename: './src/database/sqlite.db' },
+//     migrations: { directory: './src/database/migrations' },
+//     useNullAsDefault: true,
+//   },
+
+//   production: {
+//     client: 'sqlite3',
+//     connection: { filename: './src/database/sqlite.db' },
+//     migrations: { directory: './src/database/migrations' },
+//     useNullAsDefault: true,
+//   }
+// };
