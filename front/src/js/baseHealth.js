f39c767edb45d20ae1f33c7c384e69dca9a0f614
renandonun <renandonun@gmail.com>
2023-11-12 08:27:26 -0300

    update filtro prof saude

--- front/src/js/baseHealth.js
+++ front/src/js/baseHealth.js
@@ -1,6 +1,107 @@
+const _filter = (self) => {
+  const headerHealth = document.querySelector('.headerHealth')
+
+  const filterHealth = document.createElement('div')
+  filterHealth.classList.add('filterHealth')
+  headerHealth.appendChild(filterHealth)
+
+  const filterHealthSearch = document.createElement('div')
+  filterHealthSearch.classList.add('filterHealthSearch')
+  filterHealth.appendChild(filterHealthSearch)
+
+  const searchIcon = document.createElement('span')
+  searchIcon.classList.add('searchIconImg')
+  filterHealthSearch.appendChild(searchIcon)
+
+  const inputSearchHealth = document.createElement('input')
+  inputSearchHealth.classList.add('inputSearchHealth')
+  inputSearchHealth.placeholder = 'Pesquisar por...'
+  filterHealthSearch.appendChild(inputSearchHealth)
+
+  const buttonSearch = document.createElement('button')
+  buttonSearch.classList.add('buttonSearch')
+  buttonSearch.textContent = 'Buscar'
+  filterHealthSearch.appendChild(buttonSearch)
+
+  // Especialidades
+  const selectEspecialidade = document.createElement('select')
+  selectEspecialidade.classList.add('selectEspecialidade')
+  filterHealth.appendChild(selectEspecialidade)
+
+  const optionEspecialidade = document.createElement('option')
+  optionEspecialidade.value = -1
+  optionEspecialidade.textContent = 'Especialidade'
+  selectEspecialidade.appendChild(optionEspecialidade)
+
+  fetch(_addressAPI + 'specialty', { method: 'GET', headers: { "Authorization": "Bearer " + _token } })
+    .then(resp => {
+      resp.json()
+        .then(json => {
+          if (json.data) {
+            for (i in json.data) {
+              const option = document.createElement('option')
+              option.value = json.data[i].id
+              option.textContent = json.data[i].description
+              selectEspecialidade.appendChild(option)
+            }
+          }
+        })
+    })
+
+  // Cidades
+  const selectCidade = document.createElement('select')
+  selectCidade.classList.add('selectCidade')
+  filterHealth.appendChild(selectCidade)
+
+  const optionCidade = document.createElement('option')
+  optionCidade.value = 0
+  optionCidade.textContent = 'Cidade'
+  selectCidade.appendChild(optionCidade)
+
+  fetch(_addressAPI + 'city', { method: 'GET', headers: { "Authorization": "Bearer " + _token } })
+    .then(resp => {
+      resp.json()
+        .then(json => {
+          if (json.data) {
+            for (i in json.data) {
+              const option = document.createElement('option')
+              option.value = json.data[i].id
+              option.textContent = json.data[i].description
+              selectCidade.appendChild(option)
+            }
+          }
+        })
+    })
+
+  // Estados
+  const selectEstado = document.createElement('select')
+  selectEstado.classList.add('selectEstado')
+  filterHealth.appendChild(selectEstado)
+
+  const optionEstado = document.createElement('option')
+  optionEstado.textContent = 'Estado'
+  optionEstado.value = 'Estado'
+  selectEstado.appendChild(optionEstado)
+
+  fetch(_addressAPI + 'state', { method: 'GET', headers: { "Authorization": "Bearer " + _token } })
+    .then(resp => {
+      resp.json()
+        .then(json => {
+          if (json.data) {
+            for (i in json.data) {
+              const option = document.createElement('option')
+              option.value = json.data[i].state
+              option.textContent = json.data[i].state
+              selectEstado.appendChild(option)
+            }
+          }
+        })
+    })
+}
+
 const baseHealth = (self) => {
   notifyHealthCheck()
-  
+
   const divCenter = document.createElement('div')
   divCenter.classList.add('appCenter')
   divCenter.style.cssText += 'justify-content: unset; flex: .98; flex-direction: row; flex-wrap: wrap;'
@@ -37,83 +138,11 @@ const baseHealth = (self) => {
   imgUser.classList.add('imgUser')
   userInfo.appendChild(imgUser)
 
-  const filterHealth = document.createElement('div')
-  filterHealth.classList.add('filterHealth')
-  headerHealth.appendChild(filterHealth)
-
-  const filterHealthSearch = document.createElement('div')
-  filterHealthSearch.classList.add('filterHealthSearch')
-  filterHealth.appendChild(filterHealthSearch)
-
-  const searchIcon = document.createElement('span')
-  searchIcon.classList.add('searchIconImg')
-  filterHealthSearch.appendChild(searchIcon)
-
-  const inputSearchHealth = document.createElement('input')
-  inputSearchHealth.classList.add('inputSearchHealth')
-  inputSearchHealth.placeholder = 'Pesquisar por...'
-  filterHealthSearch.appendChild(inputSearchHealth)
-
-  const buttonSearch = document.createElement('button')
-  buttonSearch.classList.add('buttonSearch')
-  buttonSearch.textContent = 'Buscar'
-  filterHealthSearch.appendChild(buttonSearch)
-
-  const selectEspecialidade = document.createElement('select')
-  selectEspecialidade.classList.add('selectEspecialidade')
-  filterHealth.appendChild(selectEspecialidade)
-
-  const option1Especialidade = document.createElement('option')
-  option1Especialidade.textContent = 'Especialidade'
-  option1Especialidade.value = 'Especialidade'
-  selectEspecialidade.appendChild(option1Especialidade)
-
-  const option2Especialidade = document.createElement('option')
-  option2Especialidade.textContent = 'Cardiologista'
-  option2Especialidade.value = 'Cardiologista'
-  selectEspecialidade.appendChild(option2Especialidade)
-
-  const option3Especialidade = document.createElement('option')
-  option3Especialidade.textContent = 'Dermatologista'
-  option3Especialidade.value = 'Dermatologista'
-  selectEspecialidade.appendChild(option3Especialidade)
-
-  const option4Especialidade = document.createElement('option')
-  option4Especialidade.textContent = 'Endocrinologista'
-  option4Especialidade.value = 'Endocrinologista'
-  selectEspecialidade.appendChild(option4Especialidade)
-
-  const selectCidade = document.createElement('select')
-  selectCidade.classList.add('selectCidade')
-  filterHealth.appendChild(selectCidade)
-
-  const option1Cidade = document.createElement('option')
-  option1Cidade.textContent = 'Cidade'
-  option1Cidade.value = 'Cidade'
-  selectCidade.appendChild(option1Cidade)
-
-  const option2Cidade = document.createElement('option')
-  option2Cidade.textContent = 'São Paulo'
-  option2Cidade.value = 'São Paulo'
-  selectCidade.appendChild(option2Cidade)
-
-  const selectEstado = document.createElement('select')
-  selectEstado.classList.add('selectEstado')
-  filterHealth.appendChild(selectEstado)
-
-  const option1Estado = document.createElement('option')
-  option1Estado.textContent = 'Estado'
-  option1Estado.value = 'Estado'
-  selectEstado.appendChild(option1Estado)
-
-  const option2Estado = document.createElement('option')
-  option2Estado.textContent = 'SP'
-  option2Estado.value = 'SP'
-  selectEstado.appendChild(option2Estado)
-
   const spinner = document.createElement('div')
   spinner.classList.add('spinner')
   divCenter.appendChild(spinner)
+
+  _filter()
 
   fetch(_addressAPI + 'health', { method: 'GET', headers: { "Authorization": "Bearer " + _token } })
     .then(resp => {
